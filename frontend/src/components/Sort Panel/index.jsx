c28097ef5dedab1ed7f364ae29d625813e540a44
tsahiBarshevsky <tsahi.13@gmail.com>
2022-08-09 16:01:30 +0300

    Fixing bottom sheet flatlist problem

--- frontend/src/components/Sort Panel/index.jsx
+++ frontend/src/components/Sort Panel/index.jsx
@@ -11,13 +11,30 @@ const SortPanel = ({ sortPanelRef }) => {
 
     const onSortTypeChange = (type) => {
         setSortType(type);
+        fetch(`http://${localhost}/get-all-movies?field=${type}`)
+            .then((movies) => movies.json())
+            .then((movies) => dispatch({ type: 'SET_MOVIES', movies: movies }))
         sortPanelRef.current?.close();
-        setTimeout(() => {
-            fetch(`http://${localhost}/get-all-movies?field=${type}`)
-                .then((movies) => movies.json())
-                .then((movies) => dispatch({ type: 'SET_MOVIES', movies: movies }))
-        }, 500);
     }
+
+    const Item = ({ item }) => (
+        <TouchableOpacity
+            onPress={() => onSortTypeChange(item)}
+            style={styles.button}
+            key={item}
+        >
+            <Text style={styles.text}>{item}</Text>
+            {item === sortType &&
+                <Entypo name='check' size={20} color='black' />
+            }
+        </TouchableOpacity>
+    );
+
+    const Header = () => (
+        <View style={styles.header}>
+            <Text>Sort movies by...</Text>
+        </View>
+    );
 
     const Separator = () => (
         <View style={styles.separator} />
@@ -32,32 +49,15 @@ const SortPanel = ({ sortPanelRef }) => {
             modalStyle={styles.modalStyle}
             openAnimationConfig={{ timing: { duration: 200 } }}
             closeAnimationConfig={{ timing: { duration: 500 } }}
-            scrollViewProps={{ scrollEnabled: false }}
-        >
-            <View style={styles.bottomSheetContainer}>
-                <Text>Sort movies by...</Text>
-                <FlatList
-                    data={types}
-                    keyExtractor={(item) => item}
-                    scrollEnabled={false}
-                    renderItem={({ item }) => {
-                        return (
-                            <TouchableOpacity
-                                onPress={() => onSortTypeChange(item)}
-                                style={styles.button}
-                                key={item}
-                            >
-                                <Text style={styles.text}>{item}</Text>
-                                {item === sortType &&
-                                    <Entypo name='check' size={20} color='black' />
-                                }
-                            </TouchableOpacity>
-                        )
-                    }}
-                    ItemSeparatorComponent={Separator}
-                />
-            </View>
-        </Modalize>
+            flatListProps={{
+                data: types,
+                renderItem: Item,
+                ListHeaderComponent: Header,
+                ItemSeparatorComponent: Separator,
+                scrollEventThrottle: 4,
+                contentContainerStyle: styles.bottomSheetContainer
+            }}
+        />
     )
 }
 
@@ -67,12 +67,16 @@ export default SortPanel;
 
 const styles = StyleSheet.create({
     bottomSheetContainer: {
-        height: '100%',
-        paddingVertical: 10
+        paddingTop: 20,
+        paddingBottom: 10
     },
     modalStyle: {
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25
+    },
+    header: {
+        paddingHorizontal: 15,
+        marginBottom: 15
     },
     button: {
         flexDirection: 'row',
