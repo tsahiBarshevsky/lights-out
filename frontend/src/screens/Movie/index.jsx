3744916ff523e15c3ca7badbab8077e6d4593911
tsahiBarshevsky <tsahi.13@gmail.com>
2022-08-19 10:09:03 +0300

    Movie gradient effect; Refresh conrtol offset

--- frontend/src/screens/Movie/index.jsx
+++ frontend/src/screens/Movie/index.jsx
@@ -6,7 +6,9 @@ import moment from 'moment';
 import { useSelector } from 'react-redux';
 import { Header } from '../../components';
 import { globalStyles } from '../../utils/globalStyles';
-import { primary } from '../../utils/theme';
+import { background, primary } from '../../utils/theme';
+
+import { LinearGradient } from 'expo-linear-gradient';
 
 const format = 'DD/MM/YY HH:mm';
 const initial = { hours: 0, minutes: 0, seconds: 0, milliseconds: 0 };
@@ -41,26 +43,35 @@ const MovieScreen = ({ route }) => {
     }
 
     return (
-        <SafeAreaView style={globalStyles.container}>
-            <Header
-                caption={"Movie Detail"}
-                backFunction={() => navigation.goBack()}
-            />
-            <View style={styles.poster}>
+        <View style={globalStyles.container}>
+            <View style={{ position: 'absolute', top: 0, width: '100%', zIndex: 2 }}>
+                <Header
+                    caption={"Movie Detail"}
+                    backFunction={() => navigation.goBack()}
+                />
+            </View>
+            <View style={{ marginBottom: 10, zIndex: 1 }}>
                 <Image
                     source={{ uri: `https://image.tmdb.org/t/p/original/${movie.backdropPath}` }}
-                    style={styles.image}
+                    // style={styles.image}
+                    style={{ height: 250, width: '100%' }}
                     blurRadius={5}
                 />
-                <Text style={[styles.text, styles.title]}>{movie.title}</Text>
-                {movie.rating > 0 &&
-                    <View style={styles.rating}>
-                        <AntDesign style={styles.star} name="star" size={20} color={primary} />
-                        <Text style={[styles.text, styles.ratingCaption]}>
-                            {movie.rating}
-                        </Text>
-                    </View>
-                }
+                <LinearGradient
+                    colors={['transparent', background]}
+                    style={{ position: 'absolute', top: 0, width: '100%', height: '100%' }}
+                />
+                <View style={{ position: 'absolute', bottom: 0, paddingHorizontal: 15, paddingBottom: 25 }}>
+                    <Text style={[styles.text, styles.title]}>{movie.title}</Text>
+                    {movie.rating > 0 &&
+                        <View style={styles.rating}>
+                            <AntDesign style={styles.star} name="star" size={20} color={primary} />
+                            <Text style={[styles.text, styles.ratingCaption]}>
+                                {movie.rating}
+                            </Text>
+                        </View>
+                    }
+                </View>
             </View>
             <ScrollView
                 contentContainerStyle={styles.scrollView}
@@ -137,8 +148,108 @@ const MovieScreen = ({ route }) => {
                     </Text>
                 </TouchableOpacity>
             }
-        </SafeAreaView>
+        </View>
     )
+
+    // return (
+    //     <SafeAreaView style={globalStyles.container}>
+    //         <Header
+    //             caption={"Movie Detail"}
+    //             backFunction={() => navigation.goBack()}
+    //         />
+    //         <View style={styles.poster}>
+    //             <Image
+    //                 source={{ uri: `https://image.tmdb.org/t/p/original/${movie.backdropPath}` }}
+    //                 style={styles.image}
+    //                 blurRadius={5}
+    //             />
+    //             <Text style={[styles.text, styles.title]}>{movie.title}</Text>
+    //             {movie.rating > 0 &&
+    //                 <View style={styles.rating}>
+    //                     <AntDesign style={styles.star} name="star" size={20} color={primary} />
+    //                     <Text style={[styles.text, styles.ratingCaption]}>
+    //                         {movie.rating}
+    //                     </Text>
+    //                 </View>
+    //             }
+    //         </View>
+    //         <ScrollView
+    //             contentContainerStyle={styles.scrollView}
+    //             overScrollMode="never"
+    //         >
+    //             <View style={styles.aboutContainer}>
+    //                 <View style={styles.about}>
+    //                     <FontAwesome name="video-camera" size={17} color="white" />
+    //                     <Text style={styles.aboutTitle}>Genre</Text>
+    //                     <Text style={styles.aboutCaption}>{movie.genre}</Text>
+    //                 </View>
+    //                 <View style={styles.about}>
+    //                     <AntDesign name="clockcircle" size={17} color="white" />
+    //                     <Text style={styles.aboutTitle}>Duration</Text>
+    //                     {movie.duration > 0 ?
+    //                         <Text style={styles.aboutCaption}>
+    //                             {convertMinutesToHours(movie.duration)}
+    //                         </Text>
+    //                         :
+    //                         <Text style={styles.aboutCaption}>N/A</Text>
+    //                     }
+    //                 </View>
+    //                 <View style={styles.about}>
+    //                     <FontAwesome5 name="calendar-week" size={17} color="white" />
+    //                     <Text style={styles.aboutTitle}>Released</Text>
+    //                     <Text style={styles.aboutCaption}>{moment(movie.releaseDate).format('DD/MM/YY')}</Text>
+    //                 </View>
+    //             </View>
+    //             <Text style={[styles.text, styles.sectionTitle]}>Cast</Text>
+    //             <FlatList
+    //                 data={movie.cast}
+    //                 keyExtractor={(item) => item.cast_id}
+    //                 horizontal
+    //                 showsHorizontalScrollIndicator={false}
+    //                 style={styles.cast}
+    //                 contentContainerStyle={{ alignItems: 'flex-start' }}
+    //                 ItemSeparatorComponent={Separator}
+    //                 renderItem={({ item }) => {
+    //                     return (
+    //                         <View style={styles.actorWrapper}>
+    //                             {item.profile_path ?
+    //                                 <Image
+    //                                     source={{ uri: `https://image.tmdb.org/t/p/original${item.profile_path}` }}
+    //                                     style={styles.actorImage}
+    //                                     resizeMode="center"
+    //                                 />
+    //                                 :
+    //                                 <View style={styles.actorVector}>
+    //                                     <Entypo name="user" size={35} color="black" />
+    //                                 </View>
+    //                             }
+    //                             <Text
+    //                                 style={[styles.text, styles.actorName]}
+    //                                 numberOfLines={2}
+    //                                 ellipsizeMode="tail"
+    //                             >
+    //                                 {item.original_name}
+    //                             </Text>
+    //                         </View>
+    //                     )
+    //                 }}
+    //             />
+    //             <Text style={[styles.text, styles.sectionTitle]}>Overview</Text>
+    //             <Text style={styles.text}>{movie.overview}</Text>
+    //         </ScrollView>
+    //         {screenings.find((screening) => screening.movie.id === movie.tmdbID) &&
+    //             <TouchableOpacity
+    //                 onPress={onNavigate}
+    //                 style={styles.button}
+    //                 activeOpacity={1}
+    //             >
+    //                 <Text style={styles.buttonCaption}>
+    //                     Get Reservation
+    //                 </Text>
+    //             </TouchableOpacity>
+    //         }
+    //     </SafeAreaView>
+    // )
 }
 
 export default MovieScreen;
@@ -177,7 +288,9 @@ const styles = StyleSheet.create({
     rating: {
         flexDirection: 'row',
         justifyContent: 'flex-start',
-        alignItems: 'center'
+        alignItems: 'center',
+        // position: 'absolute',
+        // bottom: 0
     },
     ratingCaption: {
         transform: [{ translateY: 2 }]
